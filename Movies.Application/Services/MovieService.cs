dc3ab38c98c15247715ae4454d7efa2a273d1937
nakard <nakardhs@gmail.com>
2024-05-06 01:22:22 +0300

    feat: Added pagination.

--- Movies.Application/Services/MovieService.cs
+++ Movies.Application/Services/MovieService.cs
@@ -75,4 +75,9 @@ public class MovieService : IMovieService
     {
         return await _movieRepository.DeleteByIdAsync(id, token);
     }
+
+    public Task<int> GetCountAsync(string? title, int? yearOfRelease, CancellationToken token = default)
+    {
+        return _movieRepository.GetCountAsync(title, yearOfRelease, token);
+    }
 }
